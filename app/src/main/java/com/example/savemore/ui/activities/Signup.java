eea3897da9db9b71a20e4a73a1ac86da24a0de04
Vivek <123vivekverma@gmail.com>
2020-11-01 13:23:35 +0530

    Screenshots added

--- app/src/main/java/com/example/savemore/ui/activities/Signup.java
+++ app/src/main/java/com/example/savemore/ui/activities/Signup.java
@@ -1,11 +1,14 @@
 package com.example.savemore.ui.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -35,6 +38,13 @@ public class Signup extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         setContentView(R.layout.activity_signup);
 
         init();
